827824f111b860e73d59ea800249ebb58ffbf113
juha Penttinen <juha.penttinen@student.laurea.fi>
2018-04-02 15:02:07 +0300

    commit
    all

--- app_server/controllers/ctrlPelaajat.js
+++ app_server/controllers/ctrlPelaajat.js
@@ -1,5 +1,37 @@
+const request = require ('request');
+const apiURL = require ('./apiURLs');
 
 const pelaajalista = function(req, res){
+    const path = 'api/pelaajat';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+request(
+    requestOptions,
+        function (err, response, body) {
+            if (err) {
+                res.render('error', {message: err.message});
+            } else if (response.statusCode !== 200) {
+                res.render('error', {message: 'Error accessing API ' + response.statusMessage + " (" + response.statusCode+ ")" });
+            } else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            } else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            } else
+            {
+                res.render ('pelaajat', {winners: body});
+            }
+        }
+);
+
+
+};
+
+    /*
     res.render('pelaajat',{
         winners:
         [
@@ -9,6 +41,7 @@ const pelaajalista = function(req, res){
 
         ]});
 };
+*/
 module.exports = {
     pelaajalista
 };
--- app_server/controllers/ctrlStats.js
+++ app_server/controllers/ctrlStats.js
@@ -1,5 +1,35 @@
+const request = require ('request');
+const apiURL = require ('./apiURLs');
 
 const statslista = function(req, res){
+    const path = 'api/statsit';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body) {
+            if (err) {
+                res.render('error', {message: err.message});
+            } else if (response.statusCode !== 200) {
+                res.render('error', {message: 'Error accessing API ' + response.statusMessage + " (" + response.statusCode+ ")" });
+            } else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            } else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            } else
+            {
+                res.render ('stats', {winners: body});
+            }
+        }
+    );
+
+
+    /*
     res.render('stats',{
         winners:
             [
@@ -8,6 +38,7 @@ const statslista = function(req, res){
                 {arvo:'8',kuvaus:'Vuoroja pelattu', player:'The Master'}
 
             ]});
+*/
 };
 module.exports = {
     statslista
